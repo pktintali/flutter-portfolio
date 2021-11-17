c2de6d7e50cb84ee7afde45dfc6519e2d4c4726a
Pradeep Kumar <4gpradeep@gmail.com>
2021-11-17 10:12:00 +0530

    bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
+      title: 'Pradeep Kumar',
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
