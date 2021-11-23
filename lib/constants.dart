949396707227afb4e4d5c2e1d04d2b32653a3362
Pradeep Kumar <4gpradeep@gmail.com>
2021-11-23 16:17:27 +0530

    third project image :)

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,19 +7,23 @@ class Constants {
       imgURL:
           'https://raw.githubusercontent.com/pktintali/students-mitra/master/screenshots/screenshot2.png',
       projectName: 'Study Analyzer',
-      shortDescription: 'Tech stacks used react, redux, and firebase. Take test and analyze your subject knowledge with studentmitra. Key features are Recommended learning based on performance, Exam Mode and Game Mode play quiz with your friends.',
+      shortDescription:
+          'Tech stacks used react, redux, and firebase. Take test and analyze your subject knowledge with studentmitra. Key features are Recommended learning based on performance, Exam Mode and Game Mode play quiz with your friends.',
     ),
     ProjectModel(
       actionLink: '#',
       imgURL:
-          'https://raw.githubusercontent.com/pktintali/flutter-portfolio/main/assets/images/shop_guide.jpg',
-      projectName: 'Tech stacks used flutter and firebase. This App helps users to find the best shop nearby them. It also helps sellers to get more customer by listing their shop on this app. Kee features include naigation to a shop, direct call action to seller and rating.',
+          'https://raw.githubusercontent.com/pktintali/flutter-portfolio/pradeep/assets/images/shop_guide.jpg',
+      projectName: 'Local Shop Guide',
+      shortDescription:
+          'Tech stacks used flutter and firebase. This App helps users to find the best shop nearby them. It also helps sellers to get more customer by listing their shop on this app. Kee features include navigating to a shop, direct call action to seller and rating.',
     ),
     ProjectModel(
-      actionLink: '#',
+      actionLink: 'https://pksorting.web.app/',
       imgURL:
-          'https://eager-williams-af0d00.netlify.app/assets/images/greenyEarth.png',
-      projectName: 'Greeny Earth',
+          'https://raw.githubusercontent.com/pktintali/flutter-portfolio/pradeep/assets/images/sorting_visualizer.png',
+      projectName: 'Sorting Visualizer',
+      shortDescription: 'Tech stacks used flutter. Visualize sorting time of different sortting algorithms with graph. Comparision option is also available for comparing the sorting time of different - different algorithms. Best/Avg case vs Worst case comparision is also available.' 
     ),
   ];
 }
