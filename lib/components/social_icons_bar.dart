30f6398302e721c6119239273b7f68c1e2967de2
Pradeep Kumar <4gpradeep@gmail.com>
2021-11-26 18:02:54 +0530

    icon url fixed

--- lib/components/social_icons_bar.dart
+++ lib/components/social_icons_bar.dart
@@ -17,12 +17,12 @@ class SocialIconsBar extends StatelessWidget {
           children: const [
             SocialIcon(
               imgURL:
-                  'https://raw.githubusercontent.com/pktintali/flutter-portfolio/pradeep/media/icons/twitter.git',
+                  'https://raw.githubusercontent.com/pktintali/flutter-portfolio/pradeep/media/icons/twitter.gif',
               actionLink: 'https://twitter.com/pradeep_tintali',
             ),
             SocialIcon(
               imgURL:
-                  'https://raw.githubusercontent.com/pktintali/flutter-portfolio/pradeep/media/icons/instagram.git',
+                  'https://raw.githubusercontent.com/pktintali/flutter-portfolio/pradeep/media/icons/instagram.gif',
               actionLink: 'https://www.instagram.com/pradeeptintali/',
             ),
             SocialIcon(
